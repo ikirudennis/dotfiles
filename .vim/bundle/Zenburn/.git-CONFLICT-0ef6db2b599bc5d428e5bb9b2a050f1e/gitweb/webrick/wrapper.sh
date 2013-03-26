4bd2c2f193591822da43959f5e8e33ebe797c358
Dennis Burke <ikirudennis@users.noreply.github.com>
2013-03-25 22:21:19 -0400

    force zenburn to be a subrepo.

--- .vim/bundle/Zenburn/.git-CONFLICT-0ef6db2b599bc5d428e5bb9b2a050f1e/gitweb/webrick/wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# we use this shell script wrapper around the real gitweb.cgi since
-# there appears to be no other way to pass arbitrary environment variables
-# into the CGI process
-GIT_EXEC_PATH=/usr/local/git/libexec/git-core GIT_DIR=/Users/dburke/.vc/dotfiles/.vim/bundle/Zenburn/.git GITWEB_CONFIG=/Users/dburke/.vc/dotfiles/.vim/bundle/Zenburn/.git/gitweb/gitweb_config.perl
-export GIT_EXEC_PATH GIT_DIR GITWEB_CONFIG
-exec /usr/local/git/share/gitweb/gitweb.cgi
