97747b26e5c247ac71701dba29707f5a55e3ebc7
Dennis Burke <dennisburke@prodigy.net>
2024-04-21 16:32:14 -0400

    more modularizations and documentation cleanup

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -19,7 +19,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
--- "plugins" below refers to ~/.config/nvim/lua/plugins.lua
+-- "plugins" below refers to ~/.config/nvim/lua/init.lua
 require("lazy").setup("plugins")
 
 -- -----------------------------------------------------------------------------
