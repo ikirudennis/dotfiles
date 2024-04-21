d96559061b1339ffb2cd90341d5d33e7abb18932
Dennis Burke <dennisburke@prodigy.net>
2024-04-21 17:01:37 -0400

    remove an old zenburn config
    It looks like there's no similar config in the new zenburn plugin

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -149,9 +149,6 @@ if has('python3')
 	let g:gundo_prefer_python3 = 1
 endif
 
-let g:zenburn_italic_Comment=1
-let g:zenburn_enable_TagHighlight=1
-
 let g:snipMate = { 'snippet_version' : 1 }
 
 ]]
