1fa13d59e9af7cf551c587ea8a75b30f74084c71
Dennis Burke <dennisburke@prodigy.net>
2024-04-23 07:37:02 -0400

    functional ansiblels setup
    I had been struggling with getting this working, and it turned out that
    the latest ansiblels version available was broken. Upon a rollback being
    applied, and using Mason to update to that rollback version, this
    started working as intended

--- roles/neovim/files/lua/plugins/lsp-config.lua
+++ roles/neovim/files/lua/plugins/lsp-config.lua
@@ -23,6 +23,9 @@ return {
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities
 			})
+			lspconfig.ansiblels.setup({
+				capabilities = capabilities
+			})
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 			vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
