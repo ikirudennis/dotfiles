c7dc262a62f9b5f8d2c2ccc92addbeb432cfbcba
Dennis Burke <dennisburke@prodigy.net>
2024-04-28 15:31:07 -0400

    updating plugins and configuring treesitter for gitcommits
    there's still some strangeness going on when using vim-fugitive's git
    commit functionality: it's not properly highlighting the diff section.

--- roles/neovim/files/lua/plugins/treesitter.lua
+++ roles/neovim/files/lua/plugins/treesitter.lua
@@ -5,8 +5,10 @@ return {
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
 			auto_install = true,
+			ensure_installed = { "diff", "gitcommit", "git_rebase" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
 	end,
+	dependencies = { "gbprod/tree-sitter-gitcommit" },
 }
