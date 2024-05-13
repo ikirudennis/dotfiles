3a270c1d58e69d27e6715cf1943449cfbd78360f
Dennis Burke <dennisburke@prodigy.net>
2024-05-12 21:06:40 -0400

    convert vimscript to lua

--- roles/neovim/files/after/ftplugin/gitcommit.lua
+++ roles/neovim/files/after/ftplugin/gitcommit.lua
@@ -1,17 +1,18 @@
-setlocal formatoptions-=a
+-- don't fight the autoformat settings
+vim.opt_local.formatoptions:remove({"a"})
 
-" Enable spell check
-setlocal spell
+-- Enable spell check
+vim.opt_local.spell = true
 
-" Enable dictionary autocomplete
-setlocal complete+=kspell
+-- Enable dictionary autocomplete
+vim.opt_local.complete:append({'kspell'})
 
-" Wrap paragraphs at 72 characters
-setlocal tw=72
+-- Wrap paragraphs at 72 characters
+vim.opt_local.tw=72
 
-" Add cursor column showing this 72 character width
-setlocal cc=+1
+-- Add cursor column showing this 72 character width
+vim.opt_local.cc="+1"
 
-" in insert mode, use the `git jiraticket` command to insert the jira ticket
-" derived from the current branch name
-inoremap <leader>j <C-R>=trim(system('git jiraticket'))<C-M> 
+-- in insert mode, use the `git jiraticket` command to insert the jira ticket
+-- derived from the current branch name
+vim.keymap.set('i', '<leader>j', "<C-R>=trim(system('git jiraticket'))<C-M>")
