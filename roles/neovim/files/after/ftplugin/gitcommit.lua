92dcf2ddf17bc5a8e604d17b35ba496a5171a6b9
Dennis Burke <dennisburke@prodigy.net>
2024-05-12 21:15:13 -0400

    apply autoformatting from lsp

--- roles/neovim/files/after/ftplugin/gitcommit.lua
+++ roles/neovim/files/after/ftplugin/gitcommit.lua
@@ -1,18 +1,18 @@
 -- don't fight the autoformat settings
-vim.opt_local.formatoptions:remove({"a"})
+vim.opt_local.formatoptions:remove({ "a" })
 
 -- Enable spell check
 vim.opt_local.spell = true
 
 -- Enable dictionary autocomplete
-vim.opt_local.complete:append({'kspell'})
+vim.opt_local.complete:append({ "kspell" })
 
 -- Wrap paragraphs at 72 characters
-vim.opt_local.tw=72
+vim.opt_local.tw = 72
 
 -- Add cursor column showing this 72 character width
-vim.opt_local.cc="+1"
+vim.opt_local.cc = "+1"
 
 -- in insert mode, use the `git jiraticket` command to insert the jira ticket
 -- derived from the current branch name
-vim.keymap.set('i', '<leader>j', "<C-R>=trim(system('git jiraticket'))<C-M>")
+vim.keymap.set("i", "<leader>j", "<C-R>=trim(system('git jiraticket'))<C-M>")
