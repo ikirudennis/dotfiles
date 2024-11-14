97d02a0472ab3532b2d81387fd0856b726889728
Dennis Burke <dennisburke@prodigy.net>
2024-11-14 15:40:39 -0500

    add scratch keymap and improve configuration

--- roles/neovim/files/lua/plugins/scratch.lua
+++ roles/neovim/files/lua/plugins/scratch.lua
@@ -1,4 +1,13 @@
+vim.keymap.set({"n", "v"}, "<leader><Tab>", "<cmd>Scratch<cr>")
+
 return {
 	"LintaoAmons/scratch.nvim",
 	event = "VeryLazy",
+	config = function ()
+		require("scratch").setup({
+			window_cmd = "edit",
+			use_telescope = true,
+			filetypes = {"txt", "md", "py", "js", "sh", "lua"},
+		})
+	end
 }
