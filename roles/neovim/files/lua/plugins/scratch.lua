f9318edb1f45c9d72914f7f5bed8f5158584a206
Dennis Burke <dennisburke@prodigy.net>
2024-11-14 15:53:27 -0500

    remove default configurations

--- roles/neovim/files/lua/plugins/scratch.lua
+++ roles/neovim/files/lua/plugins/scratch.lua
@@ -5,8 +5,6 @@ return {
 	event = "VeryLazy",
 	config = function ()
 		require("scratch").setup({
-			window_cmd = "edit",
-			use_telescope = true,
 			filetypes = {"txt", "md", "py", "js", "sh", "lua"},
 		})
 	end
